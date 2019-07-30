5087feba82c7a6a525a04c4417306021ff482fa5
y.t <y.softvalley@gmail.com>
2019-07-30 11:51:48 +0900

    practiced vim

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/NasSilverBullet/practicegolang
 
 go 1.12
 
-require github.com/labstack/echo/v4 v4.1.6 // indirect
+require (
+	github.com/NasSilverBullet/practicegolang/testtest v0.0.0-20190730002312-08f3b4d19049 // indirect
+	github.com/labstack/echo/v4 v4.1.6 // indirect
+)
--- testvim/main.go
+++ testvim/main.go
@@ -5,6 +5,28 @@ import "fmt"
 func main() {
 	nvim := NewNeoVim()
 	fmt.Println(nvim.Parent.Parent)
+
+	hogeHuga := "HOGE"
+	fmt.Println(hogeHuga)
+
+	text := `
+These commands move slowly the cursor position.
+	`
+	fmt.Println(text)
+
+	var hoge = "HOGE"                                             // 何かのコメント
+	var fuga = "FUGA"                                             // 素敵なコメント
+	var fuganogehogehogehogehogehoge = "HOHOGAHOGEHOGEHOGEHOGHEO" // 素敵なコメント
+	fmt.Println(hoge, fuga, fuganogehogehogehogehogehoge)
+
+	longText := `
+こんな
+
+    改行と行頭空白文字混じりの
+
+        文章があったとしても一発で1行にできます
+	`
+	fmt.Println(longText)
 }
 
 // Editor indicates the type of editor.
@@ -15,7 +37,9 @@ type Editor struct {
 
 // NewEmacs is emacs constructor
 func NewEmacs() *Editor {
-	return &Editor{Name: "Emacs"}
+	return &Editor{
+		Name: "Emacs",
+	}
 }
 
 // NewVi is vi constructor
